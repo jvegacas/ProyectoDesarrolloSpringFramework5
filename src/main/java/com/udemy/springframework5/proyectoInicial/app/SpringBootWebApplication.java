c74f629b1270aa65ccf99c63e903dfe48a1d2d97
Javier Vega <javier.vega@es.gfi.world>
2020-01-22 16:24:28 +0100

    Creación del proyecto

---
+++ src/main/java/com/udemy/springframework5/proyectoInicial/app/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.springframework5.proyectoInicial.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/springframework5/proyectoInicial/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springframework5.proyectoInicial.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
